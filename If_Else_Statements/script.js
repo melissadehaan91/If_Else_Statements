3e70e000bdf0723a2636c8ae9c6a1b47c3b6df9c
melissadehaan91 <80722664+melissadehaan91@users.noreply.github.com>
2021-04-24 12:41:23 +0200

    Add files via upload

---
+++ If_Else_Statements/script.js
@@ -0,0 +1,26 @@
+const age = 18;
+let myNum = 19;
+
+if (myNum >= age){
+    console.log('You are allowed in.');
+} else{
+    console.log('Unfortunately you are not allowed in.');
+}
+
+const isFemale = true;
+
+if (isFemale){
+    console.log('You may continue to buy a ticket for the ladiesnight.');
+} else {
+    console.log('Unfortunately this evening is only for Females');
+}
+
+
+const driverStatus = 'bob';
+bob = 2;
+let myDrink = 3;
+if (myDrink <= bob){   
+    console.log('Drive safe!');
+} else {
+    console.log('You are not allowed to drive anymore, call a taxi.');
+} 
